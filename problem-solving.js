40505b201ae235ef8e16ea7baa96f2e1d91b0768
chayansrk64 <chayansrk64@gmail.com>
2026-01-16 15:52:42 +0600

    problem 2 has been solved

--- problem-solving.js
+++ problem-solving.js
@@ -4,3 +4,19 @@ function reverseString(str){
     return str.split('').reverse().join('')
 }
 reverseString('hello')
+
+// Problem 2: Count Vowels in a String
+
+function countVowel(word){
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    const chars = word.toLowerCase().split('')
+    chars.forEach(c => {
+        if(vowels.includes(c)){
+            count ++;
+        }
+    })
+    return count;
+}
+countVowel('programming');
+
